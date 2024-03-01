6995765f95eae4b4a8883a2c3b9f2d717def125e
Nicholas Solomon <48126957+nicholassolomon@users.noreply.github.com>
2024-03-01 08:15:50 -0600

    Add files via upload

--- tf_train.py
+++ tf_train.py
@@ -3,12 +3,13 @@ Training code for TF model
 """
 import tensorflow as tf
 
-def tf_model_training(model, ds_train, ds_validation, epochs)
-  history = model.fit(ds_train_ds,
+def tf_model_training(model, 
+  ds_train, ds_validation, epochs, verbose=2):
+  history = model.fit(ds_train,
                     epochs=epochs,
                     steps_per_epoch=len(ds_train),
-                    validation_data=ds_validataion,
-                    validation_steps=len(ds_validataion),
-                    verbose=2)
+                    validation_data=ds_validation,
+                    validation_steps=len(ds_validation),
+                    verbose=verbose)
   return history
   
